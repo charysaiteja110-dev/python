8e09097883d3871e5efb64f2d4ac501771da5fef
charysaiteja110-dev <charysaiteja110@gmail.com>
2026-09-19 15:20:13 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,87 @@
+print("CHAPTER-1")
+
+print("Hello world")
+print("Welcome Python")
+print("SAI TEJA CHARY RALLABANDI")
+print("I am learning python")
+print("python programming")
+print("good morning")
+print("Welcome Students")
+print("Python is easy")
+print("My first program")
+print("Hello from python!")
+
+print("CHAPTER-2")
+
+print("2 + 5")
+print(2 + 5)
+print("10 - 3")
+print(10 - 3)
+print("Hello Worl!")
+print("python")
+print("Hello")
+print("Hello")
+print("10 + 20")
+print(10 + 20)
+
+print("CHAPTER-3")
+
+name = "sai teja chary"
+age = 20
+marks = 85
+price = 99.5
+city = "chennai"
+print(city)
+score = 95
+print(score)
+x = 10
+print(x)
+message = "Welcome"
+print(message)
+a = 10
+b = 20
+print(a + b)
+quantity = 5 
+price = 10
+print(quantity * price)
+print("CHAPTER-4")
+
+print(type(25))
+print(type(25.5))
+print(type("25"))
+age = 21
+height = 5.8
+name = "Arun"
+marks = 90
+print(type(marks))
+temperature = 36.5
+print(type(temperature))
+message = "Welcome"
+print(type(message))
+
+print("CHAPTER-5")
+
+print("start"),print("Middle"),print("End")
+x = 10
+print(x)
+x = 20
+print(x)
+number = 5
+print(number)
+number = 10
+print(number)
+print("A,B,C")
+name = "Ravi"
+age = 20 
+print(name)
+print(age)
+print("1"),print("2"),print("3"),print("4")
+x = 5
+y = 10
+print(x),print(y)
+print("Start"),print(2 + 3),print("End")
+
+
+
+
+
